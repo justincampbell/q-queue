a17690fca7576550e7758476d73b7c12219eccfb
Justin Campbell <justin@justincampbell.me>
2014-08-20 17:51:54 -0400

    Run queued commands with -r

--- test/q_test.bats
+++ test/q_test.bats
@@ -1,13 +1,13 @@
 load test_helper
 
-@test "q shows usage" {
+@test '`q` shows usage' {
   run $q
 
   [ $status -eq 1 ]
   echo "$output" | grep "q-queue"
 }
 
-@test "q foo queues foo for later in ~/.q" {
+@test '`q foo` queues foo for later in ~/.q' {
   run $q foo
 
   [ $status -eq 0 ]
@@ -15,10 +15,23 @@ load test_helper
   grep "foo$" $HOME/.q
 }
 
-@test "q shows queued commands" {
+@test '`q` shows queued commands' {
   echo "$PWD	foo" > $HOME/.q
   run $q
 
   [ $status -eq 0 ]
   echo $output | grep "foo"
+}
+
+@test '`q -r` runs all queued commands' {
+  echo "$PWD	foo bar" > $HOME/.q
+
+  mock "foo"
+
+  run $q -r
+
+  [ $status -eq 0 ]
+  [ ! -f $HOME/.q ]
+  echo $output | grep $PWD
+  verify_mocks
 }
--- test/test_helper.bash
+++ test/test_helper.bash
@@ -1,14 +1,47 @@
 setup() {
-  QQ_TMPDIR=$BATS_TMPDIR/q-queue
-  HOME=$QQ_TMPDIR
-  PATH=$QQ_STUBS:$PATH
+  BATS_TMPDIR=$BATS_TMPDIR/q-queue
+  BATS_STUBS=$BATS_TMPDIR/stubs
+  BATS_MOCKS_RECEIVED=$BATS_STUBS/mocks-received
+  BATS_MOCKS_SETUP=$BATS_STUBS/mocks-setup
+  HOME=$BATS_TMPDIR
+  PATH=$BATS_STUBS:$PATH
 
-  rm -rf $QQ_TMPDIR
-  mkdir -p $QQ_TMPDIR
+  rm -rf $BATS_TMPDIR
+  mkdir -p $BATS_TMPDIR
+  mkdir -p $BATS_STUBS
 
   q=bin/q
 }
 
 teardown() {
   echo $status: $output
+}
+
+stub() {
+  stub=$BATS_STUBS/$1
+  echo "#!/bin/bash" > $stub
+  echo $2 >> $stub
+  chmod +x $stub
+}
+
+mock() {
+  touch $BATS_MOCKS_RECEIVED
+  echo $1 >> $BATS_MOCKS_SETUP
+  stub $1 "echo $1 >> $BATS_MOCKS_RECEIVED"
+}
+
+verify_mocks() {
+  cat $BATS_MOCKS_SETUP | while read mock; do
+    if ! grep $mock $BATS_MOCKS_RECEIVED; then
+      echo "$mock not called" && return 1
+    fi
+  done
+}
+
+verify_called() {
+  grep $1 $BATS_MOCKS
+}
+
+verify_not_called() {
+  grep -v $1 $BATS_MOCKS
 }
